c06051577d260a17f3bc6b1fb3f3b7446534e0a4
Julio Varas C <jvarascontreras@gmail.com>
2024-07-17 20:01:53 -0400

    Se crea clase dto para atributos. Se crea listado para renderizar en vista

--- springboot-web/src/main/java/com/first/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/first/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.first.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.ui.Model;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.first.springboot.webapp.springboot_web.models.User;
+import com.first.springboot.webapp.springboot_web.models.dto.UserDto;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -17,12 +23,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/details")
-    public Map<String, Object> details(Model model) {
+    public UserDto details(Model model) {
+        UserDto userDto = new UserDto();
         User user = new User("Julio", "VC");
-        Map<String, Object> body = new HashMap<>();
-        body.put("title","Hola Mundo SpringBoot");
-        body.put("user", user);
-        return body;
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo SpringBoot");
+        // Map<String, Object> body = new HashMap<>();
+        // body.put("title","Hola Mundo SpringBoot");
+        // body.put("user", user);
+        return userDto;
     }
 
+    @GetMapping("/list")
+    public List<User> list(){
+        User user = new User("John", "Wall");
+        User user2 = new User("Mike", "Wall");
+        User user3 = new User("Milhouse", "Wall");
+
+        // List<User> users = new ArrayList<>();
+        // users.add(user);
+        // users.add(user2);
+        // users.add(user3);
+
+        List<User> users = Arrays.asList(user,user2,user3);
+        return users;
+    }
 }
