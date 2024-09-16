2c71a81acdfa82fce3488ad7c6601ddafe86ee85
Julio Varas C <jvarascontreras@gmail.com>
2024-09-16 15:47:31 -0300

    Se crea nuevo proyecto mini de uso de un gimnasio pero con Springboot 3.3.3, JAVA 21, lombok y MySQL

---
+++ app_fit_spring/zone_fit/src/main/java/zf/zone_fit/ZoneFitApplication.java
@@ -0,0 +1,13 @@
+package zf.zone_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoneFitApplication.class, args);
+	}
+
+}
