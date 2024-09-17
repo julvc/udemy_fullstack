75a51959d2e5391ab500b6bfad3d26ad269596f3
Julio Varas C <jvarascontreras@gmail.com>
2024-09-17 12:16:02 -0300

    Se elimina y se refactoriza para iniciar proyecto con spring y primefaces

--- app_fit_primefaces_and_spring/zone_fit/src/main/java/zf/zone_fit/ZoneFitApplication.java
+++ app_fit_primefaces_and_spring/zone_fit/src/main/java/zf/zone_fit/ZoneFitApplication.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import zf.zone_fit.models.Clients;
 import zf.zone_fit.services.IClientService;
---
+++ app_fit_primefaces_and_spring/zone_fit/src/main/java/zf/zone_fit/ZoneFitWeb.java
@@ -0,0 +1,11 @@
+package zf.zone_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneFitWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(ZoneFitWeb.class, args);
+    }
+}
