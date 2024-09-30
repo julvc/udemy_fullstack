edbfa105521e3470f0404b27d073d928dec99197
Julio Varas C <jvarascontreras@gmail.com>
2024-09-30 12:07:07 -0300

    Creacion de nueva mini app de resursos humanos con React y Spring. Creacion de Modelo y conexion a base datos.

---
+++ app_react_and_spring/rrhh/src/main/java/app/rrhh/models/Employee.java
@@ -0,0 +1,25 @@
+package app.rrhh.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    String name;
+    String department;
+    Double salary;
+}
